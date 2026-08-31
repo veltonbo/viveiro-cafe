6c22219df9d62530a52730326ab0be199a1be77d
veltonbo <eliveltonoliveiranbo@gmail.com>
2026-08-30 20:59:18 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "viveiro-cache-v4";
+const CACHE = "viveiro-cache-v5";
 const ASSETS = ["./","index.html","manifest.webmanifest","icon-192.png","icon-512.png"];
 
 self.addEventListener("install", event => {
