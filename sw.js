c877cd3a130c3e2700f266e8c13e935e9e7a5aaf
veltonbo <eliveltonoliveiranbo@gmail.com>
2026-08-30 20:09:42 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,47 +1,22 @@
-const CACHE = "viveiro-cache-v1";
+const CACHE = "viveiro-cache-v2";
+const ASSETS = ["./","index.html","manifest.webmanifest","icon-192.png","icon-512.png"];
 
-const ASSETS = [
-  "./",
-  "index.html",
-  "manifest.webmanifest",
-  "icon-192.png",
-  "icon-512.png"
-];
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
 
-self.addEventListener(
-  "install",
-  event => {
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
 
-    event.waitUntil(
-
-      caches
-        .open(CACHE)
-        .then(cache =>
-          cache.addAll(ASSETS)
-        )
-
-    );
-
-  }
-);
-
-
-self.addEventListener(
-  "fetch",
-  event => {
-
-    event.respondWith(
-
-      caches
-        .match(event.request)
-        .then(resposta =>
-
-          resposta ||
-          fetch(event.request)
-
-        )
-
-    );
-
-  }
-);
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
